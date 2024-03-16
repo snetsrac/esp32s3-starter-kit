0855c4d2d48e1d60decb8ba21504813770be522f
Chris Carstens <dev@chriscarstens.com>
2024-03-16 13:31:10 -0400

    Change identifiers

--- 01.1-blink/main/main.c
+++ 01.1-blink/main/main.c
@@ -8,8 +8,8 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define GPIO_LED GPIO_NUM_2
-#define GPIO_PIN_MASK_OUTPUT (1ULL << GPIO_LED)
+#define LED GPIO_NUM_2
+#define LED_MASK (1ULL << LED)
 #define BLINK_PERIOD 1000UL
 
 /**
@@ -25,17 +25,17 @@ void print_string(const char *const s) {
  * @brief Configure GPIO pin settings.
 */
 void configure_gpio(void) {
-    gpio_config_t io_conf;
-    io_conf.pin_bit_mask = GPIO_PIN_MASK_OUTPUT;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    gpio_config(&io_conf);
+    gpio_config_t io_config;
+    io_config.pin_bit_mask = LED_MASK;
+    io_config.mode = GPIO_MODE_OUTPUT;
+    io_config.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_config.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&io_config);
 }
 
 /**
- * @brief Task: Toggle led state and print a message.
+ * @brief Task: Toggle LED state and print a message.
 */
 void vTaskToggleLed(void *pvParameters) {
     bool led_state = 0;
@@ -45,7 +45,7 @@ void vTaskToggleLed(void *pvParameters) {
     for (;;) {
         led_state = !led_state;
         print_string(led_state ? "Turning LED on." : "Turning LED off.");
-        gpio_set_level(GPIO_LED, led_state);
+        gpio_set_level(LED, led_state);
         xTaskDelayUntil(&pxPreviousWakeTime, xTimeIncrement);
     }
 }
